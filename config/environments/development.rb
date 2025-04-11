74d82f16a8d39f283c457346ee1794f0e19e228e
roy1108yor <3219973695@qq.com>
2025-04-11 12:12:31 +0800

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  # Allow requests from any domain including the Clacky environment domain
-  config.hosts.clear
 end
