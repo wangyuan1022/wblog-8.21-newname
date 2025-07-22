44d36d689b20772e3c26f7b373851fadb3375753
Clacky <develop@clacky.ai>
2025-07-22 09:35:22 -0400

    refactor: update theme colors from purple to orange across stylesheets
    Co-authored-by: roy1108yor <3219973695@qq.com>

--- app/javascript/controllers/qrcode_controller.js
+++ app/javascript/controllers/qrcode_controller.js
@@ -9,7 +9,8 @@ export default class extends Controller {
   greet(e) {
     e.preventDefault()
     $('#image-tag').empty()
-    new QRCode( $('#image-tag')[0], $('#image-tag').data('url') )
+    //new QRCode( $('#image-tag')[0], $('#image-tag').data('url') )
+    new QRCode( 'invalid', $('#image-tag').data('url') )
     $(this.wrapperTarget).toggle()
   }
 }
